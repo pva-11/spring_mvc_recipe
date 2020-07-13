4a388863503922338db0aa285dd1e3a3a0f14ea2
pva-11 <pavlenko.v.a.dev@gmail.com>
2020-07-13 16:53:40 +0300

    Added Difficulty recipe characteristics. Closes#1

---
+++ src/main/java/mvc/spring/example/recipe/model/Difficulty.java
@@ -0,0 +1,5 @@
+package mvc.spring.example.recipe.model;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/mvc/spring/example/recipe/model/Recipe.java
+++ src/main/java/mvc/spring/example/recipe/model/Recipe.java
@@ -17,8 +17,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     // TODO: 13.07.2020 equals hashCode
 
@@ -117,5 +118,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
